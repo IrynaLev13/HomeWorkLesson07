196f8ccc69dabb0b5df093fe9e6823d335dc989e
IrynaLev13 <ikot@ukr.net>
2023-10-24 18:49:33 +0300

    HW015 second commit

--- src/HW015.java
+++ src/HW015.java
@@ -2,16 +2,16 @@ public class HW015 {
     public static void main(String[] args) {
         Person person = new Person();
 
-        Person.personInfoStatic("Will", "Smith", "New York", 2936729462846L);
-        Person.personInfoStatic("Jackie", "Chan", "Shanghai", 12312412412L);
-        Person.personInfoStatic("Sherlock", "Holmes", "London", 37742123513L);
+        Person.personInfoStatic("Will ", "Smith", "New York", 2936729462846L);
+        Person.personInfoStatic("Jackie ", "Chan", "Shanghai", 12312412412L);
+        Person.personInfoStatic("Sherlock ", "Holmes", "London", 37742123513L);
 
 
 
 
-        person.personInfo("Will", "Smith", "New York", 2936729462846L);
-        person.personInfo("Jackie", "Chan", "Shanghai", 12312412412L);
-        person.personInfo("Sherlock", "Holmes", "London", 37742123513L);
+        person.personInfo("Will ", "Smith", "New York", 2936729462846L);
+        person.personInfo("Jackie ", "Chan", "Shanghai", 12312412412L);
+        person.personInfo("Sherlock ", "Holmes", "London", 37742123513L);
 
 
 
